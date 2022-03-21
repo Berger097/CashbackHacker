378b2c8ef0dcd04e8692f8f1ef1919b15e9a0545
Anatoly Moskalev <moskalev1997tolya@gmail.com>
2022-03-21 11:39:06 +0600

    new testing

--- src/test/java/ru/netology/bonus/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/bonus/CashbackHackServiceTest.java
@@ -9,8 +9,8 @@ public class CashbackHackServiceTest {
     @Test
     public void testRemain() {
         CashbackHackService service = new CashbackHackService();
-        int actual = service.remain(900);
-        int expected = 100;
+        int actual = service.remain(1000);
+        int expected = 0;
         assertEquals(actual, expected);
     }
     @Test
